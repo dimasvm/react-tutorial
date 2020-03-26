64be530298d8544dd0de5f21c07f4326e1b18bca
Dimas Maulana <araymaulana66@gmail.com>
2020-03-26 14:30:53 +0700

    Updates

---
+++ precache-manifest.591f0e1537b6dd37e93b0a6b28247bbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d0644fc62ed0bb6cac1ca54fdf1fef3",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "32062f818293f8097b81",
+    "url": "/react-tutorial/static/css/main.08011531.chunk.css"
+  },
+  {
+    "revision": "919e78dc0251c3661a37",
+    "url": "/react-tutorial/static/js/2.f876bca9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.f876bca9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32062f818293f8097b81",
+    "url": "/react-tutorial/static/js/main.d94779c1.chunk.js"
+  },
+  {
+    "revision": "48186d3ac18516ac0690",
+    "url": "/react-tutorial/static/js/runtime-main.f060380d.js"
+  }
+]);
